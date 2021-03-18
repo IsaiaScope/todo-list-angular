3ba47998eb717cf68c98fe417f66a499d2cc7949
IsaiaRiva <isaia.riva@finconsgroup.com>
2021-03-18 21:51:42 +0100

    fix server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/todolist-alone'));
+app.use(express.static('./dist/TODOLIST-ALONE'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/todolist-alone/'}),
+    res.sendFile('index.html', {root: 'dist/TODOLIST-ALONE/'}),
 );
 
 // Start the app by listening on the default Heroku port
